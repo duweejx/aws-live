7eed241c7a82b0c29c5a50beab949e345b24ead2
duweejx <143489269+duweejx@users.noreply.github.com>
2023-08-29 15:40:12 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.clswoici5vzh.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "duweejunxiang-employee"
+customregion = "us-east-1"
+
